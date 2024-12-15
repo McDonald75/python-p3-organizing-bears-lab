3c03ccc0f6979b5ffdd460f1accccc99243e3f41
McDonald75 <mcdonald.omondi@student.moringaschool.com>
2024-12-15 19:40:13 +0000

    complete

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    color TEXT,
+    temperament TEXT,
+    alive BOOLEAN,
+    sex TEXT
+)
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, color, temperament, alive, sex) VALUES ("Mr. Chocolate", 1,  NULL, NULL, TRUE, "M");
+INSERT INTO bears (name, age, color, temperament, alive, sex) VALUES ("Rowdy", 2, NULL, NULL, TRUE, "M");
+INSERT INTO bears (name, age, color, temperament, alive, sex) VALUES ("Tabitha", 3, NULL, NULL, TRUE, "M");
+INSERT INTO bears (name, age, color, temperament, alive, sex) VALUES ("Sergeant Brown",  4, NULL, NULL, TRUE, "M");
+INSERT INTO bears (name, age, color, temperament, alive, sex) VALUES ("Melissa", 5,  NULL, NULL, TRUE, "M");
+INSERT INTO bears (name, age, color, temperament, alive, sex) VALUES ("Grinch", 6,  NULL, NULL, TRUE, "M");
+INSERT INTO bears (name, age, color, temperament, alive, sex) VALUES ("Wendy", 7, NULL, NULL, TRUE, "M");
+INSERT INTO bears (name, age, color, temperament, alive, sex) VALUES (NULL, 8, NULL, NULL, TRUE, "M");
